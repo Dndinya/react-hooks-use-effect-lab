84b989766f7f10d59cc5a325663ae8f13785184a
Derrick <derrick.ndinya@student.moringaschool.com>
2024-02-07 10:54:43 +0300

    Completed assignment

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,17 @@
-import React, { useState } from "react";
+import React, { useState , useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setTimeRemaining(prevTime => prevTime - 1);
+    }, 1000);
+
+    // Cleanup function to clear the timeout
+    return () => clearTimeout(timerId);
+  }, [timeRemaining]); // Run effect whenever timeRemaining changes
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
